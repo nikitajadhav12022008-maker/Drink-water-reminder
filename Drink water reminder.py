ea52804452a0a37dfa425b81411916d3f5638051
Nikita Jadhav <nikita.jadhav12022008@gmail.com>
2026-02-08 14:07:43 +0530

    Add files via upload

---
+++ Drink water reminder.py
@@ -0,0 +1,16 @@
+from plyer import notification
+import time
+
+def drink_water_reminder():
+    notification.notify(
+        title = "💧Drink water reminder", 
+        message = "Time to drink some water and stay hydrated!",
+        timeout = 10
+    )
+
+interval = 3600
+print("Water reminder started.....")
+
+while True:
+    drink_water_reminder()
+    time.sleep(interval)
